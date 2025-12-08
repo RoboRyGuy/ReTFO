55015e19a02e209891aed30cbc3348d540f26bbd
Ryan Durland <46659883+RoboRyGuy@users.noreply.github.com>
2025-12-08 15:21:43 -0800

    Initial Commit

---
+++ Archipelago/Plugin.cs
@@ -0,0 +1,49 @@
+﻿
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ReTFO.Archipelago;
+
+[BepInPlugin(GUID, Name, Version)]
+[BepInProcess("GTFO.exe")]
+public class Plugin : BasePlugin
+{
+    public const string Name = "Archipelago";       // Plugin name
+    public const string Author = "RoboRyGuy";       // Plugin author
+    public const string GUID = $"{Author}.{Name}";  // Plugin GUID, unique identifier used by BepInEx
+    public const string Version = "1.0.0";          // Plugin version, can be used by System.Version
+
+    // Reference to plugin instance that is loaded by BepInEx
+    private static Plugin? _plugin = null;
+
+    // Instance of harmony used for patching
+    protected Harmony harmony = new(GUID);
+
+    // Get the plugin instance, throws an exception if it fails
+    public static Plugin Get() => TryGet() ?? throw new NullReferenceException($"Tried to retrieve {Name}, but it was not loaded!");
+
+    // Tries to get the plugin instance, returns null if it fails
+    public static Plugin? TryGet() => _plugin ??= IL2CPPChainloader.Instance.Plugins.FirstOrDefault(p => p.Key == GUID).Value.Instance as Plugin;
+
+    // Tries to get the plugin instance, returns true if it succeeds
+    public static bool TryGet([NotNullWhen(true)] out Plugin? plugin)
+    {
+        plugin = TryGet();
+        return plugin != null;
+    }
+
+    public override void Load()
+    {
+        _plugin = this;
+        harmony.PatchAll(GetType());
+        Log.LogInfo($"{GUID} is loaded!");
+    }
+
+    public override bool Unload()
+    {
+        harmony.UnpatchSelf();
+        return true;
+    }
+}
---
+++ Archipelago/SingleRundownPatch.cs
@@ -0,0 +1,30 @@
+﻿
+using GameData;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ReTFO.Archipelago;
+
+/// <summary>
+/// Truncates loaded Rundown GameData to ensure there's only one rundown.
+/// This forces the "Connect to Rundown" screen.
+/// </summary>
+internal class SingleRundownPatch
+{
+    [HarmonyTargetMethods]
+    public IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return AccessTools.Method(typeof(GameDataInit), nameof(GameDataInit.Initialize));
+        yield return AccessTools.Method(typeof(GameDataInit), nameof(GameDataInit.ReInitialize));
+    }
+
+    [HarmonyPostfix]
+    public void Postfix()
+    {
+        uint id = RundownDataBlock.GetAllBlocks()[0].persistentID;
+        //GameSetupDataBlock setup = GameSetupDataBlock.GetAllBlocks()[0];
+        //setup.RundownIdsToLoad = new(1);
+        //setup.RundownIdsToLoad.Add(id);
+    }
+
+}
