dbc5eca46a1b88e12fc13af251d4f1679fe9cfb8
Ryan Durland <46659883+RoboRyGuy@users.noreply.github.com>
2026-05-17 13:30:58 -0700

    Minor Edit

--- Archipelago/ModdedInstanceData/MidManager.cs
+++ Archipelago/ModdedInstanceData/MidManager.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Security.Cryptography;
